a7b1ba83a93924a22e0d5f3246dbbdc58e0c929d
mouthguardwatermelon <rmukilan115@gmail.com>
2023-03-18 20:50:42 +0800

    both login and register features created

--- app.py
+++ app.py
@@ -2,8 +2,48 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    username = None
+    password = None
+    message = ''
+    email = None
+    email_message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        with open('username.txt', 'r+') as f:
+            contents = f.read()
+            lines = contents.split(" ")
+            lines = [line for line in lines if line]
+            if username in lines:
+                message = "Username already has been taken!"
+            else:
+                f.write(f"{username} ")
+                f.seek(0)  # Reset cursor position to the beginning of the file
+        password = request.form.get('password')
+        with open('password.txt', 'r+') as x:
+            x.write(f"{password} ")
+            x.seek(0)  # Reset cursor position to the beginning of the file
+        email = request.form.get('email')
+        with open('email.txt', 'r+') as i:
+            contents = i.read()
+            emails = contents.split(" ")
+            emails = [line for line in emails if line]
+            if email in emails:
+                email_message = "Email has already been registered"
+            else:
+                i.write(f"{email} ")
+                i.seek(0)  # Reset cursor position to the beginning of the file
+        if username not in lines and email not in emails:
+            message = "Sign up successful!"
+    return render_template('sign_up.html', username=username, password=password, email=email, message=message, email_message=email_message)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     username = None
     password = None
     if request.method == 'POST':
@@ -25,7 +65,7 @@ def index():
                 return render_template('fail.html',username = username)
         else:
             return render_template('fail.html',username = username)
-    return render_template('index.html', username = username, password = password,)
+    return render_template('login.html', username = username, password = password,)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
